c7c2d80a66b3819db00e96c9c6f52f8eb796ef72
webmus <artvisionaktau@gmail.com>
2025-05-20 12:40:19 +0500

    Initial commit

---
+++ promptcombain/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>PromptCombain</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/promptcombain/assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+<?php include 'templates/nav.php'; ?>
